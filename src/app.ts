4d675bb6d004e1d829b5eebdbe9ff4594783a377
CrisCanapi730 <cristiancanapi730@gmail.com>
2026-07-08 01:51:22 -0400

    fix implementation

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ import express, { type Express, type Request, type Response } from 'express';
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response)=>{
-    res.send('Hello World');
+    res.send('Hello Worlsdd');
 });
 
 app.listen(3000);
+
+console.log("Running on port 3000");
