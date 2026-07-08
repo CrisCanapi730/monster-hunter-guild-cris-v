f1ad026341ed6d3e8c932149413100d668d3c366
CrisCanapi730 <cristiancanapi730@gmail.com>
2026-07-08 14:59:19 -0400

    add public folder and implment static function to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { type Express, type Request, type Response } from 'express';
 
 const app: Express = express();
+const port = 3000;
+
+app.use(express.static("src/public"));
 
 app.get('/', (req: Request, res: Response)=>{
-    res.send('Hello Worlsdd');
+    res.send('Hello World');
 });
 
-app.listen(3000);
-
-console.log("Running on port 3000");
+app.listen(port, ()=>{
+    console.log(`Running on port ${port}`);
+});
