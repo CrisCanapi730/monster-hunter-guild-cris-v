08a25533300aeb08c80789ee8784b64c7dbea56f
CrisCanapi730 <cristiancanapi730@gmail.com>
2026-07-10 16:46:16 -0400

    Implement interface por guild repository

---
+++ src/repositories/guild.interface.ts
@@ -0,0 +1,10 @@
+import { Guild } from "../generated/prisma/browser";
+import { Prisma } from "../generated/prisma/client";
+
+export interface IGuildRepository{
+    create(data: Prisma.GuildCreateInput): Promise<Guild>;
+    update(id: string, data: Prisma.GuildUpdateInput): Promise<Guild>;
+    delete(id: string): Promise<Guild>; 
+    findAll(): Promise<Guild>;
+    findById(id: string): Promise<Guild>;
+}
---
+++ src/repositories/guild.respository.ts
@@ -0,0 +1,6 @@
+import { Prisma, PrismaClient, Guild } from "../generated/prisma/client";
+
+
+export class GuildRepository{
+    
+}
