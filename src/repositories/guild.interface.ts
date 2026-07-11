3566ef8ad69a1662ee2f5d6dedd785aea1c0d614
CrisCanapi730 <cristiancanapi730@gmail.com>
2026-07-10 23:58:52 -0400

    repository for guild implemented

--- src/repositories/guild.interface.ts
+++ src/repositories/guild.interface.ts
@@ -5,6 +5,6 @@ export interface IGuildRepository{
     create(data: Prisma.GuildCreateInput): Promise<Guild>;
     update(id: string, data: Prisma.GuildUpdateInput): Promise<Guild>;
     delete(id: string): Promise<Guild>; 
-    findAll(): Promise<Guild>;
-    findById(id: string): Promise<Guild>;
+    findAll(): Promise<Guild[]>;
+    findById(id: string): Promise<Guild | null>;
 }
--- src/repositories/guild.respository.ts
+++ src/repositories/guild.respository.ts
@@ -5,6 +5,22 @@ import { prisma } from "../config/prisma";
 
 export class GuildRepository implements IGuildRepository{
 
-    constructor()
+    constructor(private prisma: PrismaClient){}
 
+    create(data: Prisma.GuildCreateInput): Promise<Guild>{
+        return this.prisma.guild.create({data});
+    }
+
+    update(id: string, data: Prisma.GuildUpdateInput): Promise<Guild>{
+        return this.prisma.guild.update({ where: {id}, data});
+    }
+    delete(id: string): Promise<Guild>{
+        return this.prisma.guild.delete({where: {id}});
+    }
+    findAll(): Promise<Guild[]>{
+        return this.prisma.guild.findMany();
+    }
+    findById(id: string): Promise<Guild | null>{
+        return this.prisma.guild.findUnique({where: {id}});
+    }
 }
